1485d5b4b717f24d0160400cc56e99f40a149289
6633193721 <6633193721@student.chula.ac.th>
2024-03-02 21:53:23 +0700

    authorization

--- models/Room.js
+++ models/Room.js
@@ -1,35 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const RoomSchema = new mongoose.Schema({
+const RoomSchema = new mongoose.Schema(
+  {
     roomNumber: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     price: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     maxOccupant: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     hotel: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Hotel',
-        required: true
-    }
-},
-{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hotel",
+      required: true,
+    },
+  },
+  {
     toJSON: { virtuals: true },
-    toObject: { virtuals: true }
+    toObject: { virtuals: true },
+  }
+);
+
+RoomSchema.virtual("bookings", {
+  ref: "Booking",
+  localField: "_id",
+  foreignField: "room",
+  justOne: false,
 });
 
-RoomSchema.virtual('bookings', {
-    ref: 'Booking',
-    localField: '_id',
-    foreignField: 'room',
-    justOne: false
-});
-
-module.exports = mongoose.model('Room', RoomSchema);
+module.exports = mongoose.model("Room", RoomSchema);
